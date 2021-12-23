3d9a7588b2cd73793d841e9fad7c5744d208644d
Femi Obadimu <ficazzosam@gmail.com>
2021-12-23 14:52:49 +0100

    Added stuffs

--- main.dart
+++ main.dart
@@ -1,3 +1,29 @@
+import "dart:io";
+
 void main(){
-  print('Hello World');
+  print ('Enter a Name');
+  var  name = stdin.readLineSync();
+  print ('Enter an Age');
+  var  age = stdin.readLineSync();
+
+
+  print('Hello ${name}, You are ${age} Years Old from Calculations ');
+
+  print ('Enter your Favourite Color');
+  var clr = stdin.readLineSync();
+
+  print ('Enter your Favourite Celeb');
+  var celeb = stdin.readLineSync();
+
+  print('Hello ${celeb},  ${name} favourites color is  ${clr} ');
+
+
+
+
+
+
+
+
 }
+
+
